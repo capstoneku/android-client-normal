2804a6063e631b3b084b982843b1d523b07c8c88
Chanjoo Lee <lees3301@gmail.com>
2019-04-03 01:29:54 +0900

    added Sign-in button - 협업을 고려하여 데이터바인딩은 사용하지 않음

--- app/src/main/java/kr/ac/korea/capstoneproject/SignInActivity.java
+++ app/src/main/java/kr/ac/korea/capstoneproject/SignInActivity.java
@@ -2,6 +2,8 @@ package kr.ac.korea.capstoneproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button signInBtn = findViewById(R.id.btn_sign_in);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
