a23edde4b2b04a8f3d0214712966055891df266e
Chanjoo Lee <lees3301@gmail.com>
2019-04-03 02:04:47 +0900

    added singleton for retrofit instance & dependency for json converter

---
+++ app/src/main/java/kr/ac/korea/capstoneproject/data/remote/RetrofitClient.java
@@ -0,0 +1,22 @@
+package kr.ac.korea.capstoneproject.data.remote;
+
+import kr.ac.korea.capstoneproject.BuildConfig;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = BuildConfig.SERVER_URL;
+
+    private static Retrofit mRetrofit;
+
+    public static synchronized Retrofit getInstance() {
+        if(mRetrofit == null) {
+            mRetrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return mRetrofit;
+    }
+}
