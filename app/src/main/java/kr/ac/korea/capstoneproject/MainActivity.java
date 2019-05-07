f3c286f8c3dac9e1f054d854d00790a1d584e918
zzanzu <lees3301@gmail.com>
2019-05-08 02:08:24 +0900

    added toolbar - changed theme of the activities to no actionbar - added toolbar codes to MainActivity.java - added home button icon - changed theme color to white

--- app/src/main/java/kr/ac/korea/capstoneproject/MainActivity.java
+++ app/src/main/java/kr/ac/korea/capstoneproject/MainActivity.java
@@ -2,6 +2,7 @@ package kr.ac.korea.capstoneproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_black_24dp);
     }
 }
